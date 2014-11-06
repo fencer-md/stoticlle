deb571fdc6bb14e533c95a938a1e3173f079f5b3
Max Mazur <maxim.mazur@plusplus.md>
2014-11-06 17:54:02 +0200

    Added styles for sidebar.

--- app/views/layouts/backend/sidebar.blade.php
+++ app/views/layouts/backend/sidebar.blade.php
@@ -1,17 +1,89 @@
 <ul class="page-sidebar-menu">
     @if ( Auth::user()->role == "1" )
-		<li class="avlb-amount">Users: {{ $data['usersRegistered'] }}</li>
-		<li class="avlb-amount">Money in the system: {{ $data['currentAmmount'] }}$</li>
-		<li class="avlb-amount">Total invested: {{ $data['totalInvested'] }}$</li>
-		<li class="avlb-amount">Total rewarded: {{ $data['totalRewarded'] }}$</li>
-		<li class="avlb-amount">Cycles: {{ $data['totalCycles'] }}</li>
+		<li class="dashboard-stat purple-plum avlb-amount">
+			<div class="visual">
+				<i class="fa fa-globe"></i>
+			</div>
+			<div class="details">
+				<div class="number">{{ $data['usersRegistered'] }}</div>
+				<div class="desc">Users</div>
+			</div>
+		</li>
+		<li class="dashboard-stat purple-plum avlb-amount">		
+			<div class="visual">
+				<i class="fa fa-globe"></i>
+			</div>
+			<div class="details">
+				<div class="number">{{ $data['currentAmmount'] }}$</div>
+				<div class="desc">Money in the system</div>
+			</div>
+		</li>
+		<li class="dashboard-stat purple-plum avlb-amount">		
+			<div class="visual">
+				<i class="fa fa-globe"></i>
+			</div>
+			<div class="details">
+				<div class="number">{{ $data['totalInvested'] }}$</div>
+				<div class="desc">Total invested</div>
+			</div>
+		</li>
+		<li class="dashboard-stat purple-plum avlb-amount">		
+			<div class="visual">
+				<i class="fa fa-globe"></i>
+			</div>
+			<div class="details">
+				<div class="number">{{ $data['totalRewarded'] }}$</div>
+				<div class="desc">Total rewarded</div>
+			</div>
+		</li>
+		<li class="dashboard-stat purple-plum avlb-amount">		
+			<div class="visual">
+				<i class="fa fa-globe"></i>
+			</div>
+			<div class="details">
+				<div class="number">{{ $data['totalCycles'] }}</div>
+				<div class="desc">Cycles</div>
+			</div>
+		</li>
 	@endif
     @if ( Auth::user()->role == "2" )
 		<li class="name">Hello {{ Auth::user()->userInfo->first_name }}</li>
-		<li class="avlb-amount">Available amount: {{ $data['currentAmmount'] }}$</li>
-		<li class="total-invested">Total invested: {{ $data['totalInvested'] }}$</li>
-		<li class="total-rewarded">Total rewarded: {{ $data['totalRewarded'] }}$</li>
-		<li class="current-invested">Current invested: {{ $data['lastInvestedAmmount'] }}$</li>
+		<li class="dashboard-stat purple-plum avlb-amount">
+			<div class="visual">
+				<i class="fa fa-globe"></i>
+			</div>
+			<div class="details">
+				<div class="number">{{ $data['currentAmmount'] }}$</div>
+				<div class="desc">Available ammount</div>
+			</div>
+		</li>
+		<li class="dashboard-stat purple-plum avlb-amount">
+			<div class="visual">
+				<i class="fa fa-globe"></i>
+			</div>
+			<div class="details">
+				<div class="number">{{ $data['totalInvested'] }}$</div>
+				<div class="desc">Total invested</div>
+			</div>
+		</li>
+		<li class="dashboard-stat purple-plum avlb-amount">
+			<div class="visual">
+				<i class="fa fa-globe"></i>
+			</div>
+			<div class="details">
+				<div class="number">{{ $data['totalRewarded'] }}$</div>
+				<div class="desc">Total rewarded</div>
+			</div>
+		</li>
+		<li class="dashboard-stat purple-plum avlb-amount">
+			<div class="visual">
+				<i class="fa fa-globe"></i>
+			</div>
+			<div class="details">
+				<div class="number">{{ $data['lastInvestedAmmount'] }}$</div>
+				<div class="desc">Current invested</div>
+			</div>
+		</li>
     	@if ( Auth::user()->awaiting_award == "1" )
 			<li class="cycle-end">At the end of the cycle you will recieve: @include('includes.backend.cycles')</li>
 		@endif
