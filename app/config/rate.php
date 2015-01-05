23bc28258f80578cb1ae98e7ce01f0748a40432d
sysrex server <server@sysrex.com>
2015-01-05 12:53:54 +0100

    added some new files

--- app/config/rate.php
+++ app/config/rate.php
@@ -1,5 +1,6 @@
-<?php
-		return array(
-'rate' => 0.007692307692,
-'days' => 26,
+<?php return array (
+  'original_rate' => '20',
+  'days' => '26',
+  'rate' => 0.0076923076923076927,
+  'min' => '50',
 );
