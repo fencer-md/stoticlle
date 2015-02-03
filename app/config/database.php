0fe95bd251e47c12ba81dcebfe2ddfba88b9c5ed
eugen.stribitchi@plusplus.md <admin@localhost>
2015-02-03 18:43:35 +0000

    Server chnage

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'stoticlle',
-			'username'  => 'root',
-			'password'  => 'zosima13',
+			'database'  => 'admin_jarvis',
+			'username'  => 'admin_jarvis',
+			'password'  => 'str787ef!',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
