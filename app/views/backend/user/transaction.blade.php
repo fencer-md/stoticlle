426a71d1ebc6517d1bec229c46309650f05f45a3
MadMind <alex.madmind@gmail.com>
2015-03-16 20:26:36 +0200

    Fixed login error for users without announcements.

--- app/views/backend/user/transaction.blade.php
+++ app/views/backend/user/transaction.blade.php
@@ -3,28 +3,34 @@
 @include('announcements.common.stream-helpers')
 
 @section('content')
-    <h3 class="page-title">User Chat</h3>
+    @if($stream)
+        <h3 class="page-title">User Chat</h3>
 
-    <div class="col-md-12">
-        <div class="clearfix">
-            <form class="pull-left form-inline clearfix">
-                <div class="form-group user-chart-form">
-                    <label for="account-sum">Сумма на счету</label>
-                    <input type="text" class="form-control" id="account-sum">
-                </div>
-            </form>
+        <div class="col-md-12">
+            <div class="clearfix">
+                <form class="pull-left form-inline clearfix">
+                    <div class="form-group user-chart-form">
+                        <label for="account-sum">Сумма на счету</label>
+                        <input type="text" class="form-control" id="account-sum">
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
 
-    <div class="col-md-12 stream-wrapper">
-        {? $grouped = $stream->groupedByDate('d.m.Y', $user) ?}
-        @include('announcements.common.stream')
-        <div class="package-footer">
-            <div class="package-footer-header">Всего ставок:</div>
-            <div class="package-footer-body">{{$grouped['count']}}</div>
-            <div class="package-footer-footer"><img src="{{asset('images/calendar.png')}}" alt="calendar"></div>
+        <div class="col-md-12 stream-wrapper">
+            {? $grouped = $stream->groupedByDate('d.m.Y', $user) ?}
+            @include('announcements.common.stream')
+            <div class="package-footer">
+                <div class="package-footer-header">Всего ставок:</div>
+                <div class="package-footer-body">{{$grouped['count']}}</div>
+                <div class="package-footer-footer"><img src="{{asset('images/calendar.png')}}" alt="calendar"></div>
+            </div>
         </div>
-    </div>
+    @else
+        <div class="col-md-12">
+            Подпишитесь на рассылку анонсов!
+        </div>
+    @endif
 @stop
 
 @section('custom_scripts')
