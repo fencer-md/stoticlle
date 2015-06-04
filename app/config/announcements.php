c6a1375f6219d6421e462fea2629378839cbbadb
Fencer <eugen.stribitchi@plusplus.md>
2015-06-04 18:40:42 +0000

    Current version

---
+++ app/config/announcements.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  'duration' => '7',
+  'expiryReminder1' => '2',
+  'expiryReminder2' => '1',
+);
---
+++ app/lang/ro/news.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    'latest' => '<div> Lansăm Jarvis!</div>
+    <div>Cumpără cu $25 pînă joi o săptămînă în Jarvis. Prin PayPal sau WebMoney</div>',
+);
